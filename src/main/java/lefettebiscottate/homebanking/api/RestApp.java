15ad92648a5b2f516cedeeb34da5f8a86c0fa84f
Andrea <kustomx@hotmail.it>
2020-06-18 11:55:48 +0200

    Aggiunta dependency per javax nel pom.xml. Creata la classe RestApp.java

---
+++ src/main/java/lefettebiscottate/homebanking/api/RestApp.java
@@ -0,0 +1,10 @@
+package lefettebiscottate.homebanking.api;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class RestApp extends Application 
+{
+   
+}
