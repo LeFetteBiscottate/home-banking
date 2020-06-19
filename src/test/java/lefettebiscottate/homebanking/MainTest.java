509f10e889357e13f094cc03ed0548049cbf5993
Andrea <kustomx@hotmail.it>
2020-06-19 15:23:56 +0200

    Disabilitata la classe test e aggiunta la classe dummy UserResource.java

--- src/test/java/lefettebiscottate/homebanking/MainTest.java
+++ src/test/java/lefettebiscottate/homebanking/MainTest.java
@@ -19,40 +19,41 @@ import lefettebiscottate.homebanking.entity.UserEntity;
 class MainTest {
 
 	@Test
+	@Disabled
 	void test_insertUserIntoDB() {
-		String name = "John";
-		String surname = "Denver";
-		LocalDate birthdate = LocalDate.of(1970, 01, 01);
-		String email = "johndenver@gmail.com";
-		String password = "t4kemeh0me";
-		String phonenumber = "1234567890";
-		String fiscal_code = "DNVJHN70A01C773Y";
-		Gender gender = Gender.M;
-		AccountType account_type = AccountType.CLIENT;
-		Boolean isRegistrato = true;
-		String partita_IVA = null;
-		
-		String via = "Via Michelangelo Vattelappesca";
-		String civico = "45/A";
-		String comune = "Roma";
-		String provincia = "Roma";
-		String stato = "Italia";
-		int cap = 00123;
-		
-		UserEntity johnDenver = new UserEntity(name, surname, email, birthdate, password, phonenumber, fiscal_code, gender, account_type, partita_IVA, isRegistrato);
-		AddressEntity indirizzoDiJohn = new AddressEntity(via, civico, comune, provincia, stato, cap, johnDenver);
-		
-		UserDao<UserEntity, Integer> ud = new UserDao<>();
-		
-		Future<UserEntity> fu = ud.insert(johnDenver);
-		
-		try {
-			fu.get();
-		} catch (InterruptedException | ExecutionException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+//		String name = "John";
+//		String surname = "Denver";
+//		LocalDate birthdate = LocalDate.of(1970, 01, 01);
+//		String email = "johndenver@gmail.com";
+//		String password = "t4kemeh0me";
+//		String phonenumber = "1234567890";
+//		String fiscal_code = "DNVJHN70A01C773Y";
+//		Gender gender = Gender.M;
+//		AccountType account_type = AccountType.CLIENT;
+//		Boolean isRegistrato = true;
+//		String partita_IVA = null;
+//		
+//		String via = "Via Michelangelo Vattelappesca";
+//		String civico = "45/A";
+//		String comune = "Roma";
+//		String provincia = "Roma";
+//		String stato = "Italia";
+//		int cap = 00123;
+//		
+////		UserEntity johnDenver = new UserEntity(name, surname, email, birthdate, password, phonenumber, fiscal_code, gender, account_type, partita_IVA, isRegistrato);
+////		AddressEntity indirizzoDiJohn = new AddressEntity(via, civico, comune, provincia, stato, cap, johnDenver);
+////		
+////		UserDao<UserEntity, Integer> ud = new UserDao<>();
+//		
+////		Future<UserEntity> fu = ud.insert(johnDenver);
+//		
+//		try {
+////			fu.get();
+//		} catch (InterruptedException | ExecutionException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		
 	}
 
 }
